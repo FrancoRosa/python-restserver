3ec8ecfe2858ca29d9e99855998b4d0a4d833466
FrancoRosa <km115.franco@gmail.com>
2020-08-23 09:41:45 -0500

    Add basic API behavior

--- restserver.py
+++ restserver.py
@@ -1,25 +1,28 @@
+from flask import request
+from flask import abort
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
+about = {
+        'author': u'Gared',
+        'methods': u'Insert, Bubble, Ramdom'
+        }
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/solver', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'about': about})
 
+@app.route('/solver', methods=['POST'])
+def create_task():
+    if 'file' in request.json:
+        print(request.json['file'])
+        return jsonify({'file': True}), 201
+    if 'method' in request.json:
+        print(request.json['method'])
+        print(request.json['samples'])
+        return jsonify({'solve': True}), 201
+    return abort(404)
 if __name__ == '__main__':
     app.run(debug=True)
+
